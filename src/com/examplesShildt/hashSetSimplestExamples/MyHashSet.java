3b3fe63ed6cb1bc062e5c7f4db01cefaa3fcdb60
Oksana <pasom@bigmir.net>
2016-10-07 15:14:32 +0400

    commit

---
+++ src/com/examplesShildt/hashSetSimplestExamples/MyHashSet.java
@@ -0,0 +1,17 @@
+package com.examplesShildt.hashSetSimplestExamples;
+
+import java.util.HashSet;
+
+public class MyHashSet {
+
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<>();
+        hs.add("String");
+        hs.add("TEST");
+        hs.add("testTEST");
+        hs.add("SomeString");
+
+        System.out.println(hs);
+    }
+}
+
