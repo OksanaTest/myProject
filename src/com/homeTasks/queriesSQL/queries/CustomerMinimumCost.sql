9f8dcab7ed9a36e15c0dddd3305abd89571e8a7f
Oksana <pasom@bigmir.net>
2016-11-21 01:56:59 +0300

    SQLqueries

--- src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
+++ src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
@@ -4,12 +4,12 @@ LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
 WHERE company_name = 'Luxoft'
-GROUP BY customer_name HAVING sum(cost) <= ALL(SELECT SUM(cost) FROM customer_projects cp
+GROUP BY project_name HAVING sum(cost) <= ALL(SELECT SUM(cost) FROM customer_projects cp
 LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'Luxoft' GROUP BY customer_name)
+WHERE company_name = 'Luxoft' GROUP BY project_name)
 
 UNION
 
@@ -24,7 +24,7 @@ LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'Lohika' GROUP BY customer_name)
+WHERE company_name = 'Lohika' GROUP BY project_name)
 
 UNION
 
@@ -39,4 +39,4 @@ FROM customer_projects cp LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id 
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'SBT Systems' GROUP BY customer_name);
+WHERE company_name = 'SBT Systems' GROUP BY project_name);
