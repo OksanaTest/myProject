c22afebbbfd3fd1936c7aed3edf9d2e5cbab4ac0
Oksana <pasom@bigmir.net>
2016-11-21 04:00:40 +0300

    SQLqueries

--- src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
+++ src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
@@ -1,45 +1,45 @@
 /*
 Найти клиента (customer), который приносит меньше всего прибыли компании (company) для каждой из компаний
 */
-SELECT SUM(cost),project_name,company_name, customer_name FROM customer_projects cp
+SELECT SUM(cost), company_name, customer_name FROM customer_projects cp
 LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
 WHERE company_name = 'Luxoft'
-GROUP BY project_name HAVING sum(cost) <= ALL(SELECT SUM(cost) FROM customer_projects cp
+GROUP BY customer_name HAVING sum(cost) <= ALL(SELECT SUM(cost) FROM customer_projects cp
 LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'Luxoft' GROUP BY project_name)
+WHERE company_name = 'Luxoft' GROUP BY customer_name)
 
 UNION
 
-SELECT SUM(cost),project_name,company_name, customer_name FROM customer_projects cp
+SELECT SUM(cost), company_name, customer_name FROM customer_projects cp
 LEFT JOIN projects p ON cp.projects_id = p.project_id 
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'Lohika'
+WHERE customer_name = 'Lohika'
 GROUP BY project_name HAVING SUM(cost) <= ALL(SELECT SUM(cost) FROM customer_projects cp 
 LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'Lohika' GROUP BY project_name)
+WHERE company_name = 'Lohika' GROUP BY customer_name)
 
 UNION
 
-SELECT SUM(cost),project_name,company_name, customer_name FROM customer_projects cp 
+SELECT SUM(cost), company_name, customer_name FROM customer_projects cp
 LEFT JOIN projects p ON cp.projects_id = p.project_id
 LEFT JOIN customer c ON cp.customer_id = c.customer_id
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'SBT Systems'
+WHERE customer_name = 'SBT Systems'
 GROUP BY project_name HAVING SUM(cost) <= ALL(SELECT SUM(cost)
 FROM customer_projects cp LEFT JOIN projects p ON cp.projects_id = p.project_id 
 LEFT JOIN customer c ON cp.customer_id = c.customer_id 
 LEFT JOIN companies_projects copmp ON cp.projects_id = copmp.project_id
 LEFT JOIN companies ON copmp.company_id = companies.companies_id
-WHERE company_name = 'SBT Systems' GROUP BY project_name);
+WHERE company_name = 'SBT Systems' GROUP BY customer_name);
