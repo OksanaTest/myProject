1a788a815c80e5740c1dc932b9916422aae6e2e2
Oksana <pasom@bigmir.net>
2016-11-26 08:21:23 +0300

    SQL

--- src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
+++ src/com/homeTasks/queriesSQL/queries/CustomerMinimumCost.sql
@@ -1,8 +1,8 @@
 /*
 Найти клиента (customer), который приносит меньше всего прибыли компании (company) для каждой из компаний
 */
-SELECT b.ProjectMinCost,b.company_name , t.customer_name FROM(
-SELECT MIN(cost) AS ProjectMinCost, a.* FROM (
+SELECT b.project_min_cost, b.company_name , t.customer_name FROM(
+SELECT MIN(cost) AS project_min_cost, a.* FROM (
 SELECT SUM(cost) AS cost, company_name
 FROM companies_projects_customer cpc
 INNER JOIN customer c ON cpc.customer_id = c.customer_id
@@ -21,4 +21,4 @@ INNER JOIN companies cp ON cpc.companies_id = cp.companies_id
 INNER JOIN projects p ON cpc.project_id = p.project_id
 GROUP BY  company_name, customer_name) t
 
-ON t.cost = b.ProjectMinCost AND t.company_name = b.company_name;
+ON t.cost = b.project_min_cost AND t.company_name = b.company_name;
